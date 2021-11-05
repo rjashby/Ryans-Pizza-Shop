9353e66d9417b421249b9fef481bcdcc71229f19
Ryan Ashby <rjapsu160@gmail.com>
2021-11-05 10:10:20 -0700

    create pizza.cost method starting at base price of 0 and having 4 ways to increment cost up.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+//Business Logic
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+}
+
+Pizza.prototype.cost = function () {
+  let priceTotal = 0
+  let sizeAdd = 0
+  let toppingOneAdd = 0
+  let toppingTwoAdd = 0
+  let toppingThreeAdd = 0
+
+  
+}
+
+//UI Logic
